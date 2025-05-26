ecbc3b17f3421a8dd49501ae2730fdf75ca47efc
onpux <onputrevor1@gmail.com>
2025-05-26 18:28:44 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,28 @@
+// src/components/Navbar.jsx
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
-      <a className="navbar-brand" href="/">Mi Portafolio</a>
+      <Link className="navbar-brand" to="/">Mi Portafolio</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
         <span className="navbar-toggler-icon"></span>
       </button>
-      
+
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ms-auto">
           <li className="nav-item">
-            <a className="nav-link" href="#inicio">Inicio</a>
+            <Link className="nav-link" to="/">Inicio</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#proyectos">Proyectos</a>
+            <Link className="nav-link" to="/todo">ToDo App</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#contacto">Contacto</a>
+            <Link className="nav-link" to="/weather">Clima</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/calculator">Calculadora</Link>
           </li>
         </ul>
       </div>
