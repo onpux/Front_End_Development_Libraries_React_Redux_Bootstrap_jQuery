81203c2bc11a6d4f4835f53e1c1705e9b4b46779
onpux <onputrevor1@gmail.com>
2025-05-26 18:46:30 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 import { HashRouter } from 'react-router-dom';
+import './styles/global.css'; // o tu archivo principal de estilos
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
