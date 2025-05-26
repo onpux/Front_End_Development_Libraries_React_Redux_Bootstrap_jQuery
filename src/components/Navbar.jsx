d05c50ee7d5d0c36cf530e1d188ebbfa5b3f47c9
onpux <onputrevor1@gmail.com>
2025-05-26 18:45:13 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-// src/components/Navbar.jsx
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -6,7 +5,12 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
       <Link className="navbar-brand" to="/">Mi Portafolio</Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
 
@@ -16,13 +20,16 @@ const Navbar = () => {
             <Link className="nav-link" to="/">Inicio</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/todo">ToDo App</Link>
+            <Link className="nav-link" to="/app1">App 1</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/weather">Clima</Link>
+            <Link className="nav-link" to="/app2">App 2</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/calculator">Calculadora</Link>
+            <Link className="nav-link" to="/app3">App 3</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/contacto">Contacto</Link>
           </li>
         </ul>
       </div>
