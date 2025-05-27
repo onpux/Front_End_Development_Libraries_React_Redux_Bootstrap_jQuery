4a63e44c0bc1bb7a0311f1dd4e5f6725e11b5e27
onpux <onputrevor1@gmail.com>
2025-05-27 13:17:06 -0500

    Delete src directory

--- src/index.js
+++
@@ -1,5 +0,0 @@
-// /src/index.js
-import './styles/variables.css';
-import './styles/globals.css';
-import './styles/overrides.css';
-import './styles/layout.css';
--- src/projects/ToDoApp/index.js
+++
@@ -1,8 +0,0 @@
-// /src/projects/ToDoApp/index.js
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
