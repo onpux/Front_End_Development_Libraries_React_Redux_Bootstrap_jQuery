7ef0dc6297aed1111c823cda234707113898be55
onpux <onputrevor1@gmail.com>
2025-05-30 12:20:33 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
