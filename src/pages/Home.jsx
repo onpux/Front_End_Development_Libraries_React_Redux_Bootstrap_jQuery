2bd83bf3820ae9bb2bdc17dcd7a761e06dd3c5a0
onpux <onputrevor1@gmail.com>
2025-05-30 12:12:59 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,21 @@
-// /src/components/Dashboard.jsx
-import React, { useEffect, useState } from "react";
+import React from "react";
+import Calculator from "../components/Calculator";
+import Dashboard from "../components/Dashboard";
+import ToDoList from "../components/ToDoList";
 
-const Dashboard = () => {
-  const [datos, setDatos] = useState(null);
+const Home = () => (
+  <div>
+    <h1>Bienvenido a mi portafolio SPA</h1>
+    <section>
+      <Calculator />
+    </section>
+    <section>
+      <Dashboard />
+    </section>
+    <section>
+      <ToDoList />
+    </section>
+  </div>
+);
 
-  useEffect(() => {
-    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
-      .then((res) => res.json())
-      .then((data) => setDatos(data.bpi));
-  }, []);
-
-  return (
-    <div className="card p-3">
-      <h3>Dashboard Criptomonedas</h3>
-      {datos ? (
-        <ul className="list-group">
-          {Object.keys(datos).map((key) => (
-            <li key={key} className="list-group-item">
-              {key}: {datos[key].rate}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Cargando datos...</p>
-      )}
-    </div>
-  );
-};
-
-export default Dashboard;
+export default Home;
