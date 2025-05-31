382ca8523b80b70fb1b3dd0291bebd0e1eb4f55f
onpux <onputrevor1@gmail.com>
2025-05-31 18:19:23 -0500

    Add files via upload

---
+++ scr/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ToDoApp from './todoapp/ToDoApp';
+import Calculadora from './calculadora/Calculadora';
+import DashboardClima from './dashboardclima/DashboardClima';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <Routes>
+        <Route path="/" element={<h1>Portafolio Frontend</h1>} />
+        <Route path="/todoapp" element={<ToDoApp />} />
+        <Route path="/calculadora" element={<Calculadora />} />
+        <Route path="/dashboardclima" element={<DashboardClima />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
